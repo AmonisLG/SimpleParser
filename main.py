580e41e2f592aa85f79019cd0982e67737cf9f68
AmonisLG <ledzhinov@gmail.com>
2023-04-10 18:37:06 +0300

    самый простой парсер

---
+++ main.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+url = [
+    "https://habr.com/ru/articles/728154/"
+]
+
+file = open('text.txt', 'a')
+
+for x in url:
+    html_code = str(urlopen(x).read(), 'utf8')
+    soup = BeautifulSoup(html_code, 'html.parser')
+
+    s = soup.find('title').text
+    file.write(s + '\n')
+    p = soup.find_all('p')
+    print(s)
+    for i in p:
+        print(i.text)
+        file.write(i.text+ '\n')
+file.close()
+
+
+
